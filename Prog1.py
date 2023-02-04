e92ab16f3612e4e27c2d819c2ac6ebbd5a69202e
DanielLob-o <124405596+DanielLob-o@users.noreply.github.com>
2023-02-04 14:21:50 +0100

    Esto es una modificación donde me saco la picha

--- Prog1.py
+++ Prog1.py
@@ -1,4 +1,3 @@
-"""
 def showAnimal(name,n_legs):
     print(f"El animal {name} tiene {n_legs} patas.")
 
@@ -25,15 +24,13 @@ def FunComplex(Multiplicacion,Modulus,num1,num2):
     return(Multiplicacion(num1,num2),Modulus(num1,num2))
 
 print(FunComplex(Multiplicacion,Modulus,100,20))
-"""
-"""
+
 def login(name,email="Sin determinar"):
     print(f"Bienvenido {name} {email}")
 
 login("Juan")
 login("Juan","kenaa@example.com")
-"""
-"""
+
 def SumaIterada(number):
     suma=0
     i=0
@@ -44,21 +41,18 @@ def SumaIterada(number):
     
 print(SumaIterada(10))
 print(SumaIterada(100))
-"""
 #Funciones anónimas
 #Funciones lambda
-"""
+
 Cuadrado=lambda parametro: parametro**2
 print(Cuadrado(5))
 print(Cuadrado(25))
-"""
-"""
+
 #Funcion lambda que retorna un boolean
 Mayorque999=lambda num: num>999
 print(Mayorque999(10))
-"""
-"""
+
 mult=lambda num1,num2:num1*num2
 
 print(mult(20,15))
-"""
+
